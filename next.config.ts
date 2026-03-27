543e7f2fb49490e9ecaf78d097a5827833b7e844
idhsSite <kailashupchar.private@gmail.com>
2026-03-27 20:38:27 +0530

    feat: implement SSR and draftbotpro SEO strategy for homepage, fix Auth.js redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.mylegalnotice.in',
+          },
+        ],
+        destination: 'https://mylegalnotice.in/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
